3c0fe6b76a17266ad45a64e613bd773782f9d339
Fatih20 <fnri39@protonmail.com>
2025-03-23 14:22:32 +0700

    feat: fix contact us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import ContactUs from "../components/about/ContactUs";
 
 function AboutPage() {
   return (
-    <main className="flex flex-col items-center justify-start w-full bg-gradient-to-b from-red-bright to-pink-dark min-h-screen pt-28 md:pt-40 pb-10">
+    <main className="flex flex-col items-center justify-start w-full bg-gradient-to-b from-red-bright to-pink-dark min-h-screen pt-28 md:pt-40">
       <div className="max-w-7xl px-8 flex flex-col w-full gap-6">
         <section className="flex flex-col items-center justify-start gap-4">
           <h1 className="text-transparent bg-clip-text bg-gradient-to-b from-soft-beige to-pink-light font-extrabold text-4xl md:text-7xl text-center font-plus-jakarta drop-shadow-2xl">
@@ -81,7 +82,9 @@ function AboutPage() {
             partnerships with the domestic and international institutions.
           </p>
         </section>
+        <div className="border-b-2 border-white w-full"></div>
       </div>
+      <ContactUs />
     </main>
   );
 }
