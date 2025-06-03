5be63f640302aa4a7213c552e3e7ae19a0a1a8eb
Fatih20 <13521060@std.stei.itb.ac.id>
2025-06-03 08:48:42 +0700

    feat: changed the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import WelcomingRemarks from "./components/home/WelcomingRemarks";
 import ContactUs from "./components/home/ContactUs";
 import GallerySection from "./components/home/GallerySection";
 import Sponsors from "./components/home/Sponsors";
-import bundaranHIPhoto from "@/assets/bundaran-HI-mini.webp";
+import landingPagePhoto from "@/assets/Landing-page-mini.webp";
 
 function HomePage() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-start bg-gradient-to-b from-red-dark to-red-bright">
-      <HeaderPhoto photoUrl={bundaranHIPhoto.src}>
+      <HeaderPhoto overlayOpacity={0.3} photoUrl={landingPagePhoto.src}>
         <div className="w-full h-full flex flex-col items-center justify-center font-plus-jakarta gap-2 box-border px-8 md:px-0">
           <h1 className="text-white text-center text-2xl md:text-7xl font-extrabold animate-fade-in opacity-0">
             JAKARTA INTERNATIONAL
