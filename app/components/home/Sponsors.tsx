983ed4605a1953d14a63d6e8a516dc00e9cb5e98
Fatih20 <13521060@std.stei.itb.ac.id>
2025-08-05 12:59:36 +0700

    feat: add remaining sponsors

--- app/components/home/Sponsors.tsx
+++ app/components/home/Sponsors.tsx
@@ -6,32 +6,55 @@ import ptsmi from "@/assets/sponsors-and-medpars/ptsmi.png";
 import pegadaian from "@/assets/sponsors-and-medpars/pegadaian.png";
 import lorena from "@/assets/sponsors-and-medpars/lorena.png";
 import bankJakarta from "@/assets/sponsors-and-medpars/bank-jakarta.png";
+import petrochina from "@/assets/sponsors-and-medpars/petrochina.png";
+import pln from "@/assets/sponsors-and-medpars/pln.png";
+import jamkrindo from "@/assets/sponsors-and-medpars/jamkrindo.png";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const tiers = ["Bronze", "Silver", "Gold", "Platinum"] as const;
+
+type Tier = (typeof tiers)[number];
 
 // Mock sponsor data - in a real application, this would likely come from a data file or API
 const sponsors = [
   {
-    name: "Globy",
-    logo: globy,
-    tier: "Platinum",
-  },
-  {
-    name: "PT SMI",
-    logo: ptsmi,
-    tier: "Platinum",
-  },
-  {
-    name: "Pegadaian",
-    logo: pegadaian,
-    tier: "Platinum",
+    name: "Petrochina",
+    logo: petrochina,
+    tier: "Bronze",
   },
   {
     name: "Lorena",
     logo: lorena,
-    tier: "Platinum",
+    tier: "Bronze",
   },
   {
     name: "Bank Jakarta",
     logo: bankJakarta,
+    tier: "Bronze",
+  },
+  {
+    name: "Globy",
+    logo: globy,
+    tier: "Bronze",
+  },
+  {
+    name: "PT SMI",
+    logo: ptsmi,
+    tier: "Bronze",
+  },
+  {
+    name: "Pegadaian",
+    logo: pegadaian,
+    tier: "Bronze",
+  },
+  {
+    name: "PLN",
+    logo: pln,
+    tier: "Platinum",
+  },
+  {
+    name: "Jamkrindo",
+    logo: jamkrindo,
     tier: "Platinum",
   },
 ] as SponsorLogoProps[];
@@ -39,13 +62,13 @@ const sponsors = [
 interface SponsorLogoProps {
   name: string;
   logo?: StaticImageData;
-  tier: string;
+  tier: Tier;
 }
 
 function SponsorLogo({ name, logo, tier }: SponsorLogoProps) {
   // Set dimensions based on tier
-  const dimensions = {
-    Platinum: { container: "h-48 w-48", image: "h-40 w-full" },
+  const dimensions: Record<Tier, { container: string; image: string }> = {
+    Platinum: { container: "h-48 w-fit", image: "w-64 h-64" },
     Gold: { container: "h-40", image: "h-32 w-32" },
     Silver: { container: "h-36", image: "h-28 w-28" },
     Bronze: { container: "h-32", image: "h-24 w-24" },
@@ -56,7 +79,7 @@ function SponsorLogo({ name, logo, tier }: SponsorLogoProps) {
 
   return (
     <div
-      className={`p-4 rounded-lg flex items-center justify-center ${container} justify-self-center self-center w-full`}
+      className={`p-4 rounded-lg flex items-center justify-center ${container} justify-self-center self-center`}
     >
       <div className={`relative ${image}`}>
         {/* Use a fallback div with the name if no logo is provided */}
@@ -85,22 +108,10 @@ function Sponsors() {
         Our Sponsors
       </h2>
 
-      <div className="container py-4 w-full">
+      <div className="container py-4 w-full flex flex-col items-center justify-center">
         {/* <h3 className="text-white text-xl md:text-2xl font-bold mb-4 text-center">
           Platinum Partners
         </h3> */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-          {sponsors
-            .filter((s) => s.tier === "Platinum")
-            .map((sponsor, index) => (
-              <SponsorLogo
-                key={index}
-                name={sponsor.name}
-                logo={sponsor.logo}
-                tier="Platinum"
-              />
-            ))}
-        </div>
 
         {/* <h3 className="text-white text-xl md:text-2xl font-bold mb-4 text-center">
           Gold Partners
@@ -137,7 +148,7 @@ function Sponsors() {
         {/* <h3 className="text-white text-xl md:text-2xl font-bold mb-4 text-center">
           Bronze Partners
         </h3> */}
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-items-center">
           {sponsors
             .filter((s) => s.tier === "Bronze")
             .map((sponsor, index) => (
@@ -146,6 +157,18 @@ function Sponsors() {
                 name={sponsor.name}
                 logo={sponsor.logo}
                 tier="Bronze"
+              />
+            ))}
+        </div>
+        <div className="flex flex-wrap justify-center gap-6 mb-8 w-full">
+          {sponsors
+            .filter((s) => s.tier === "Platinum")
+            .map((sponsor, index) => (
+              <SponsorLogo
+                key={index}
+                name={sponsor.name}
+                logo={sponsor.logo}
+                tier="Platinum"
               />
             ))}
         </div>
